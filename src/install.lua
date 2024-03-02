902184311ce09d5759844fc1190a1ac3fad8bb51
tcklpl <negroniluan@gmail.com>
2024-03-02 15:40:04 -0300

    Install script now reboots the computer
    Co-authored-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
    Co-authored-by: Jader <jadergedeon@gmail.com>
    Co-authored-by: Kesley <Kesley-Amorim@users.noreply.github.com>

--- src/install.lua
+++ src/install.lua
@@ -1,4 +1,5 @@
 local shell = require("shell")
+local computer = require("computer")
 local args = {...}
 local repo = "https://raw.githubusercontent.com/OperRat-Technologies/power-station"
 local branch
@@ -20,3 +21,5 @@ end
 for i = 1, #scripts do
     shell.execute(string.format("wget -f %s/%s/src/%s", repo, branch, scripts[i]))
 end
+
+computer.shutdown(true)
