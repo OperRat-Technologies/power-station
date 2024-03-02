f6cef67c6f9944522e8a22594e3aad658cbd73cd
tcklpl <negroniluan@gmail.com>
2024-03-01 23:24:39 -0300

    Created an object to hold the data and added more info
    Co-authored-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>

--- src/main.lua
+++ src/main.lua
@@ -1,17 +1,68 @@
 
 -- Configuration
-local readingMeanValues = 5
+local readingTickInterval = 20
 
-
+-- Module imports
 local term = require("term")
 local component = require("component")
 local gpu = component.gpu
--- 
--- Useful methods for the substation
--- getEUStored
--- getEUCapacity
--- getEUOutputAverage / getEUInputAverage
-local substation, lapotronic_supercapacitor
+
+local GregTechMachine = { }
+GregTechMachine.new = function (proxy, inStr, outStr)
+    local self = {}
+
+    self.proxy = proxy
+    self.sensorInfo = proxy.getSensorInformation()
+    self.inStr = inStr
+    self.outStr = outStr
+
+    function self.searchSensorInformation(searchStr)
+        for _, v in ipairs(self.sensorInfo) do
+            if string.find(v, searchStr) then
+                return v
+            end
+        end
+        return ""
+    end
+
+    function self.updateSensorInfo()
+        self.sensorInfo = proxy.getSensorInformation()
+    end
+
+    ---Clears colored strings
+    ---@param str string
+    ---@return string
+    function self.clearSensorInformationString(str)
+        local result, _ = string.gsub(str, "§.", "")
+        return result
+    end
+
+    function self.extractNumberFromInformationString(str)
+        local result = string.gsub(str, ",", "")
+        local _, _, number = string.find(result, "(%d+)")
+        return tonumber(number)
+    end
+
+    function self.getEUStored()
+        return self.proxy.getEUStored()
+    end
+    
+    function self.getEUCapacity()
+        return self.proxy.getEUCapacity()
+    end
+    
+    function self.getEUAverageInput()
+        return self.extractNumberFromInformationString(self.clearSensorInformationString(self.searchSensorInformation(self.inStr)))
+    end
+
+    function self.getEUAverageOutput()
+        return self.extractNumberFromInformationString(self.clearSensorInformationString(self.searchSensorInformation(self.outStr)))
+    end
+    
+    return self
+end
+
+local substationProxy, lapotronicProxy
 
 -- loop through all components in the system
 for address, name in component.list() do
@@ -21,14 +72,17 @@ for address, name in component.list() do
         local machineName = proxy.getName()
 
         if (string.find(machineName, "substation")) then
-            substation = proxy
+            substationProxy = proxy
         end
 
         if (string.find(machineName, "supercapacitor")) then
-            lapotronic_supercapacitor = proxy
+            lapotronicProxy = proxy
         end
     end
 end
+
+local substation = GregTechMachine.new(substationProxy, "Average Input", "Average Output")
+local lapotronic = GregTechMachine.new(lapotronicProxy, "Avg EU IN", "Avg EU OUT")
 
 ---Style setup
 local function setupScreen()
@@ -101,15 +155,19 @@ local function printSubstationGraphic(psCapacity, psStorage)
     printCapacityGraphic(psCapacity, psStorage, 10, 46, 3)
 end
 
-local function printScreen(lsCapacity, lsStorage, lsInEU, lsOutEU, psCapacity, psStorage)
+local function ticksToHHMMSS(ticks)
+    local seconds = math.floor(ticks / 20)
+    local hours = math.floor(seconds / 3600)
+    local minutes = math.floor((seconds - (hours * 3600)) / 60)
+    local seconds = seconds - (hours * 3600) - (minutes * 60)
+    return hours, minutes, seconds
+end
 
-    local lsPercentage = lsStorage / lsCapacity * 100
+local function choice(c, t, f)
+    return c and t or f
+end
 
-    -- number of layers on the graphic below (the │ ░░░░░ │ lines)
-    local gLayers = 20
-    local percentagePerLayer = 100 / gLayers
-
-    local status = lsInEU > lsOutEU and "   CHARGING" or "DISCHARGING"
+local function printScreen(lsCapacity, lsStorage, psCapacity, psStorage, tickLife, inEu, outEu)    
 
     local lsCapVal, lsCapMod = numToAdaptedScientificNotation(lsCapacity)
     local psCapVal, psCapMod = numToAdaptedScientificNotation(psCapacity)
@@ -117,6 +175,14 @@ local function printScreen(lsCapacity, lsStorage, lsInEU, lsOutEU, psCapacity, p
     local lsStoVal, lsStoMod = numToAdaptedScientificNotation(lsStorage)
     local psStoVal, psStoMod = numToAdaptedScientificNotation(psStorage)
 
+    local lifeH, lifeM, lifeS = ticksToHHMMSS(math.abs(tickLife))
+    
+    local euDiff = math.abs(inEu - outEu)
+    local inEUVal, inEUMod = numToAdaptedScientificNotation(inEu)
+    local outEUVal, outEUMod = numToAdaptedScientificNotation(outEu)
+    local diffEUVal, diffEUMod = numToAdaptedScientificNotation(euDiff)
+
+    local untilFullyString = choice(inEu > outEu, "charged   ", "discharged")
 
     print(string.format("  ┌───────┐                                ┌───────┐                            "))
     print(string.format("  │       │   Lapotronic Supercapacitor    │       │   Power Substation         "))
@@ -128,73 +194,54 @@ local function printScreen(lsCapacity, lsStorage, lsInEU, lsOutEU, psCapacity, p
     print(string.format("  │       │   ┌ Storage ───────────────┐   │       │ ┌ Storage ───────────────┐ "))
     print(string.format("  │       │   │ %18.2f %sEU │   │       │ │ %18.2f %sEU │ ", lsStoVal, lsStoMod, psStoVal, psStoMod))
     print(string.format("  │       │   └────────────────────────┘   │       │ └────────────────────────┘ "))
-    print(string.format("  │       │                              ╔»│       │                            "))
-    print(string.format("  │       │   ┌ Stats ─────────────────┐ ║ └───────┘                            "))
-    print(string.format("  │       │   │                        │ ║                                      "))
-    print(string.format("  │       │   │ Status:    %s │ ║                                      ", status))
-    print(string.format("  │       │   │ Percentage:   %6.2f %s | ║                                      ", lsPercentage, "%"))
-    print(string.format("  │       │   │ In:  %11.2f %sEU/t │ ║                                      ", numToAdaptedScientificNotation(lsInEU)))
-    print(string.format("  │       │   │ Out: %11.2f %sEU/t │ ║                                      ", numToAdaptedScientificNotation(lsOutEU)))
-    print(string.format("  │       │   └────────────────────────┘ ║                                      "))
-    print(string.format("  │       ╞»═════════════════════════════╝                                      "))
-    print(string.format(" ╔╧═══════╧╗                                                                    "))
-    print(string.format(" ╚════O════╝                                                                    "))
+    print(string.format("  │       │ ╔═════════════════════════════»│       │      .---.                 "))
+    print(string.format("  │       │ ║                              └───────┘ (\\./)     \\.......-        "))
+    print(string.format("  │       │ ║                                        >' '<  (__.'\"\"\"\"BP         "))
+    print(string.format("  │       │ ║ ┌ Stats ───────────────────────────────\"-`-\"-\"──────────────────┐ "))
+    print(string.format("  │       │ ║ │                                                               │ "))
+    print(string.format("  │       │ ║ │ Θ Battery Life:     %02d:%02d:%02d until fully %s           │ ", lifeH, lifeM, lifeS, untilFullyString))
+    print(string.format("  │       │ ║ │ ↑ Charging:     %6.2f %sEU/t                                  │ ", inEUVal, inEUMod))
+    print(string.format("  │       ╞»╝ │ Δ Difference:   %6.2f %sEU/t                                  │ ", diffEUVal, diffEUMod))
+    print(string.format("  │       │   │ ↓ Discharging:  %6.2f %sEU/t                                  │ ", outEUVal, outEUMod))
+    print(string.format(" ╔╧═══════╧╗  │                                                               │ "))
+    print(string.format(" ╚════O════╝  └───────────────────────────────────────────────────────────────┘ "))
 
     printLapotronicGraphic(lsCapacity, lsStorage)
     printSubstationGraphic(psCapacity, psStorage)
 end
 
----Calculate the mean of a list of values
----@param values table
-local function meanWithout0(values)
-    local sum = 0
-    local dividend = 1
-    for _,v in pairs(values) do
-        sum = sum + v
-        if (v > 0) then
-            dividend = dividend + 1
-        end
-    end
-    return sum / dividend
-end
-
-local cap_last_5_input, cap_last_5_output = {}, {}
-
 setupScreen()
+
+local substationCapacity = substation.getEUCapacity()
+local lapotronicCapacity = lapotronic.getEUCapacity()
+local totalEUCapacity = substationCapacity + lapotronicCapacity
 
 while true do
 
-    local sub_stored = substation.getEUStored()
-    local sub_capacity = substation.getEUCapacity()
-    -- substation returns 0 for both input and output
+    substation.updateSensorInfo()
+    lapotronic.updateSensorInfo()
+    local substationStored = substation.getEUStored()
+    local lapotronicStored = lapotronic.getEUStored()
 
-    local cap_stored = lapotronic_supercapacitor.getEUStored()
-    local cap_capacity = lapotronic_supercapacitor.getEUCapacity()
-    local cap_input = lapotronic_supercapacitor.getEUInputAverage()
-    local cap_output = lapotronic_supercapacitor.getEUOutputAverage()
-    local cap_einput = lapotronic_supercapacitor.getAverageElectricInput()
-    local cap_eoutput = lapotronic_supercapacitor.getAverageElectricOutput()
-
-    local cap_actual_input = cap_input ~= 0 and cap_input or cap_einput
-    local cap_actual_output = cap_output ~= 0 and cap_output or cap_eoutput
-
-    table.insert(cap_last_5_input, cap_actual_input)
-    table.insert(cap_last_5_output, cap_actual_output)
-
-    if (#cap_last_5_input > readingMeanValues) then
-        table.remove(cap_last_5_input, 1)
-    end
-    if (#cap_last_5_output > readingMeanValues) then
-        table.remove(cap_last_5_output, 1)
+    local euSum = substationStored + lapotronicStored
+    local euIn = lapotronic.getEUAverageInput()
+    local euOut = substation.getEUAverageOutput()
+    local euPerTickDiff = euIn - euOut
+    local tickLife = -1
+    if (euPerTickDiff < 0) then
+        tickLife = euSum / math.abs(euPerTickDiff)
+    else
+        tickLife = (totalEUCapacity - euSum) / euPerTickDiff
     end
 
-    local ls_input = meanWithout0(cap_last_5_input)
-    local ls_output = meanWithout0(cap_last_5_output)
 
     term.clear()
 
     printBanner()
-    printScreen(cap_capacity, cap_stored, ls_input, ls_output, sub_capacity, sub_stored)
+    printScreen(lapotronicCapacity, lapotronicStored, substationCapacity, substationStored, tickLife, euIn, euOut)
+
+---@diagnostic disable-next-line: undefined-field
+    os.sleep(20 / readingTickInterval)
 
 end
 
