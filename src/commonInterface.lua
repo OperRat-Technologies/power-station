0c50f9dd96712fe173a4cb07e0c26fbc895902f2
tcklpl <negroniluan@gmail.com>
2024-03-04 15:25:33 -0300

    added a few more comments

--- src/commonInterface.lua
+++ src/commonInterface.lua
@@ -15,15 +15,19 @@ function CIParam(row, col, length, format)
         format = format
     }
 
+    ---Sets the cursor to the start of the param on the screen
     function self.setCursor()
         term.setCursor(col, row)
     end
     
+    ---Prints the format string with the supplied arguments
+    ---@param ... any
     function self.print(...)
         self.setCursor()
         term.write(string.format(self.format, ...), false)
     end
 
+    ---Clears the output string
     function self.clear()
         gpu.fill(self.col, self.row, self.length, 1, " ")
     end
@@ -53,6 +57,7 @@ function CIScreen(rows, cols, bgColor, fgColor, ...)
         gpu.setViewport(self.cols, self.rows)
     end
 
+    ---Prints the background in a non-wrapping way
     function self.printBackground()
         self.assertScreenSize()
         for i = 1, #self.background do
@@ -61,11 +66,15 @@ function CIScreen(rows, cols, bgColor, fgColor, ...)
         end
     end
 
+    ---Registers a new Param to the screen
+    ---@param param any
+    ---@return any
     function self.registerParam(param)
         table.insert(self.params, param)
         return param
     end
 
+    ---Clears the screen for all registered params
     function self.clearAllParams()
         for i = 1, #self.params do
             self.params[i].clear()
