ebf8dbd59b6ea198b52e2eb054a8fbf6b6eece95
Jader <jadergedeon@gmail.com>
2024-05-21 17:12:01 -0300

    Added turbine logic

--- src/turbine.lua
+++ src/turbine.lua
@@ -2,20 +2,27 @@ local component = require("component")
 local config = require("config")
 
 local Turbine = {}
-Turbine.new = function(wirelessFrequency, machine)
+Turbine.new = function(wirelessFrequency, totalEuCapacity)
     local self = {}
 
     self.wirelessFrequency = wirelessFrequency
-    self.machine = machine
+    self.totalEuCapacity = totalEuCapacity
     self.enabled = true
 
     component.redstone.setWirelessFrequency(wirelessFrequency)
     component.redstone.setWirelessOutput(false)
 
-    function self.updateTurbine()
-        if self.enabled then
-            local psPercentage = (self.machine.getEUStored() / self.machine.getEUCapacity()) * 100
-            component.redstone.setWirelessOutput(psPercentage < config.alarmPowerPercentageThreshold)
+    function self.updateTurbine(totalEUStored)
+        if not self.enabled then
+            return
+        end
+            
+        local psPercentage = (totalEUStored / self.totalEuCapacity) * 100
+            
+        if psPercentage < minPowerPercentageThreshold then
+            component.redstone.setWirelessOutput(true)
+        elseif psPercentage > maxPowerPercentageThreshold then
+            component.redstone.setWirelessOutput(false)
         end
     end
 
